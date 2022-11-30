c2d32b229e0cb4854fae89e35922d152f8a35060
willianggilloni <willian.gilloni@mercadolivre.com>
2022-11-30 09:41:06 -0300

    Finalizando projeto

--- lojagames/src/main/java/com/generation/games/model/Categoria.java
+++ lojagames/src/main/java/com/generation/games/model/Categoria.java
@@ -1,5 +1,56 @@
 package com.generation.games.model;
 
-public class Categoria {
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_categoria")
+
+public class Categoria {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Tipo da categoria é obrigatório")
+    @Size(min = 3 , max = 20, message="Digite no mínimo 3 caracteres e no máximo 20 caracteres")
+    private String tipo;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+    private List<Produto> produtos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
 }
--- lojagames/src/main/java/com/generation/games/repository/ProdutoRepository.java
+++ lojagames/src/main/java/com/generation/games/repository/ProdutoRepository.java
@@ -1,5 +1,22 @@
 package com.generation.games.repository;
 
-public class ProdutoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.games.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Long> {
+
+    public List<Produto> findAllByDescricaoContainsIgnoreCase(@Param("descricao") String descricao);
+
+    public List<Produto> findAllByValorGreaterThanOrderByValor(Double valor);
+
+    public List<Produto> findAllByValorIsLessThanOrderByValor(Double valor);
+
+
 
 }
